dc1cedf0d81f8198b17a844b919b6a63199f52e7
Julia-Markelova <usikmarkelova@mail.ru>
2018-11-04 15:00:15 +0300

    main objects are here

---
+++ restaurant.py
@@ -0,0 +1,24 @@
+
+class Table:
+    def __init__(self, size):
+        self.size = size
+        self.available = True
+
+
+class Waiter:
+    def __init__(self, service_time):
+        self.service_time = service_time
+        self.available = True
+
+
+class Cooker:
+    def __init__(self, cooking_time):
+        self.size = size
+        self.cooking_time = cooking_time
+
+
+class Request:
+    def __init__(self, size, probability, eating_time):
+        self.size = size
+        self.probability = probability
+        self.eating_time = eating_time
